649c872d446cc8af8fdf178a8f10f42b630ebf24
Rohit Kamble <rohitkamble171012@gmail.com>
2025-09-25 06:58:34 +0530

    Update server.js

--- SmartLearn_Backend/src/server.js
+++ SmartLearn_Backend/src/server.js
@@ -22,11 +22,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(compression());
-app.use(cors({
+/*app.use(cors({
   origin: process.env.CLIENT_URL, 
   credentials: true
-}));
-//app.use(cors({origin:true , credentials : true}))
+}));*/
+app.use(cors({origin:true , credentials : true}))
 
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ extended: true }));
