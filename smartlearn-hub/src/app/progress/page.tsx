e292b5fda7f6155051f4e506f780210c0ffad4b2
Rohit Kamble <rohitkamble171012@gmail.com>
2025-09-26 07:48:22 +0530

    Update page.tsx

--- smartlearn-hub/src/app/progress/page.tsx
+++ smartlearn-hub/src/app/progress/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { getProgress } from "@/lib/quizApi";
-import { useTheme } from "@/context/ThemeContext";
+import { getProgress } from "../../lib/quizApi";
+import { useTheme } from "../../context/ThemeContext";
 import {
   BarChart,
   Bar,
