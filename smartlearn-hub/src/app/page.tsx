161dfdb0529222a01a5c773ceb7b6a173b503fd9
Rohit Kamble <rohitkamble171012@gmail.com>
2025-09-26 08:26:24 +0530

    Update page.tsx

--- smartlearn-hub/src/app/page.tsx
+++ smartlearn-hub/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
-import { useTheme } from "@/context/ThemeContext";
+import { useTheme } from "../context/ThemeContext";
 
 export default function Home() {
   const { theme } = useTheme(); // ✅ Context state (no reload)
